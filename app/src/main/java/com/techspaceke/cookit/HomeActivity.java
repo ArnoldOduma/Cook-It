f6cfe10a5d36fd97159b2878211c8aafa90fdf2f
Arnold Oduma <arnoldcliff98@gmail.com>
2019-07-21 23:34:31 +0300

    This commit will: 1-> Create a cook activity 2-> Add a string description to be embed in the layout 3-> Bind the views using butterknife to make the different views available for manipulation 4-> Create an onclick action for the buttons in the home layout to move to the next intent 4-> Link the various fonts to the different text views

---
+++ app/src/main/java/com/techspaceke/cookit/HomeActivity.java
@@ -0,0 +1,83 @@
+package com.techspaceke.cookit;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.Layout;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.helloTextView) TextView mHelloTextView;
+    @BindView(R.id.helloDescTextView) TextView mHelloDescTextView;
+    @BindView(R.id.tryButton) Button mTryOutButton;
+
+
+    private View mControlsView;
+    private final Runnable mShowPart2Runnable = new Runnable() {
+        @Override
+        public void run() {
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.show();
+            }
+            mControlsView.setVisibility(View.VISIBLE);
+        }
+    };
+
+    GridView gridView;
+    String[] description = new String[]{
+            "If you've wanted to learn how to make crispy fried chicken, this is the recipe for you. " +
+                    "Always a picnic favorite, this deep fried chicken recipe is delicious either hot or cold. " +
+                    "Kids call it my Kentucky Fried Chicken!"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+//        setContentView(R.layout.trending_item);
+
+
+         ButterKnife.bind(this);
+
+        //Set Click listeners
+        mTryOutButton.setOnClickListener(this);
+
+        gridView = findViewById(R.id.baseGridView);
+        TrendingAdapter trendingAdapter = new TrendingAdapter(this, description);
+        gridView.setAdapter(trendingAdapter);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        mHelloTextView.setText("Hello " + name + " ,");
+
+
+        Typeface caviarDreams = Typeface.createFromAsset(getAssets(),"fonts/caviar_dreams.ttf");
+        Typeface bodoni = Typeface.createFromAsset(getAssets(),"fonts/bodoni.ttf");
+        mHelloDescTextView.setTypeface(caviarDreams);
+        mHelloTextView.setTypeface(bodoni);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == mTryOutButton){
+            Intent intent = new Intent(HomeActivity.this,CookActivity.class);
+            startActivity(intent);
+        }
+    }
+}
