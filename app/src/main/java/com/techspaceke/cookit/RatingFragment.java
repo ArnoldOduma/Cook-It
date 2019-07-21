680d372ae0e0ed1dc7faaf797ef14127a4468671
Arnold Oduma <arnoldcliff98@gmail.com>
2019-07-21 23:58:44 +0300

    This commit will create a rating fragment class which will host the rating stars to be used in rating the recipe

---
+++ app/src/main/java/com/techspaceke/cookit/RatingFragment.java
@@ -0,0 +1,27 @@
+package com.techspaceke.cookit;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import butterknife.BindView;
+
+public class RatingFragment {
+//    @BindView(R.id.rateTextView)
+//    TextView mRateTextView;
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstancesState){
+//        View rootView = inflater.inflate(R.layout.fragment_rate, container, false);
+//
+//
+//        getDialog().setTitle("Rate Us");
+//
+//        return rootView;
+//    }
+}
