769548f2cc80523b5d3b152619f16de0a1852695
Arnold Oduma <arnoldcliff98@gmail.com>
2019-07-29 21:03:28 +0300

    This commit will: 1->create constants to be used in the api call service to prevent typo mistakes and also save on reusing code

---
+++ app/src/main/java/com/techspaceke/cookit/Constants.java
@@ -0,0 +1,7 @@
+package com.techspaceke.cookit;
+
+public class Constants {
+
+    public static final String MEALDB_BASE_URL = "https://www.themealdb.com/api/json/v1/1/search.php";
+    public static final String MEALDB_NAME_QUERY_PARAMETER = "s";
+}
