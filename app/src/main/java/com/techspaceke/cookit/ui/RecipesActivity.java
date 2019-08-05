45c41ef8484906fa3b201cc8065b53b838d86472
Arnold Oduma <arnoldcliff98@gmail.com>
2019-08-06 02:14:31 +0300

    This commit will: 1-> Create an exception listener to throw errors to user and also to redirect user incase an action is not found

--- app/src/main/java/com/techspaceke/cookit/ui/RecipesActivity.java
+++ app/src/main/java/com/techspaceke/cookit/ui/RecipesActivity.java
@@ -1,14 +1,28 @@
 package com.techspaceke.cookit.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ActionBar;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.techspaceke.cookit.R;
 import com.techspaceke.cookit.adapters.RecipeListAdapter;
@@ -16,14 +30,18 @@ import com.techspaceke.cookit.models.Recipes;
 import com.techspaceke.cookit.services.RecipeService;
 
 import java.io.IOException;
+import java.sql.Time;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Timer;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.github.inflationx.calligraphy3.CalligraphyConfig;
 import io.github.inflationx.calligraphy3.CalligraphyInterceptor;
 import io.github.inflationx.viewpump.ViewPump;
+import io.github.inflationx.viewpump.ViewPumpContextWrapper;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
@@ -33,6 +51,8 @@ public class RecipesActivity extends AppCompatActivity {
     private static final String TAG = RecipesActivity.class.getSimpleName();
     @BindView(R.id.recyclerView) RecyclerView mRecyclerView ;
     @BindView(R.id.recipeKeyedResult) TextView mRecipeKeyedResult;
+    @BindView(R.id.progressBar2) ProgressBar mProgressBar;
+    @BindView(R.id.my_toolbar) Toolbar mToolBar;
     private RecipeListAdapter mAdapter;
     public List<Recipes> recipesList = new ArrayList<>();
 
@@ -41,49 +61,109 @@ public class RecipesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes);
         ButterKnife.bind(this);
+
+        setSupportActionBar(mToolBar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mToolBar.getOverflowIcon().setColorFilter(Color.BLACK, PorterDuff.Mode.SRC_ATOP);
+
+
         Intent intent = getIntent();
         String meal = intent.getStringExtra("meal");
         mRecipeKeyedResult.setText("Results for " + meal);
         getRecipes(meal);
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())){
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            getRecipes(meal);
+        }
+
+        showProgressDialog();
 
         Typeface scope = Typeface.createFromAsset(getAssets(),"fonts/scope.ttf");
         mRecipeKeyedResult.setTypeface(scope);
 
-        //import font
 
+
+        //import font
         ViewPump.init(ViewPump.builder()
                 .addInterceptor(new CalligraphyInterceptor(
                         new CalligraphyConfig.Builder()
-                                .setDefaultFontPath("fonts/bodoni.ttf")
+                                .setDefaultFontPath("fonts/sans_pro.ttf")
                                 .setFontAttrId(R.attr.fontPath)
                                 .build()))
                 .build());
     }
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase));
+    }
 
+    private void timeOut(){
+
+    }
     private void getRecipes(String meal){
         RecipeService.findRecipes(meal, new Callback() {
+
             @Override
             public void onFailure(Call call, IOException e) {
+                RecipesActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(RecipesActivity.this,e.getLocalizedMessage()+"Error !!!" ,Toast.LENGTH_LONG).show();
+                        String httpError = e.getMessage();
+                        mRecipeKeyedResult.setTextColor(Color.parseColor("red"));
+                        mRecipeKeyedResult.setTextSize(TypedValue.COMPLEX_UNIT_DIP,20);
+
+                        if (httpError.contains("address")){
+                            mRecipeKeyedResult.setText("Unable to resolve host !!!\n Please check your internet connection and try again");
+                        }else {
+                            mRecipeKeyedResult.setText(e.getLocalizedMessage() + "Error !!!");
+                        }
+                        mRecipeKeyedResult.setText(e.getLocalizedMessage());
+                        Date date = new Date();
+                        int time = date.getMinutes();
+
+                        Toast.makeText(RecipesActivity.this,time+"Error !!!" ,Toast.LENGTH_LONG).show();
+//                        if (time = )
+                        hideProgressDialog();
+                    }
+                });
                 e.printStackTrace();
             }
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {//
-//                ResponseBody responseBody = response.peekBody(Long.MAX_VALUE);
-//                String jsonData = responseBody.string();
-//                Log.e(TAG, jsonData);
-                recipesList = RecipeService.processResults(response);
-                RecipesActivity.this.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        mAdapter = new RecipeListAdapter(getApplicationContext(), recipesList);
-                        mRecyclerView.setAdapter(mAdapter);
-                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(RecipesActivity.this);
-                        mRecyclerView.setLayoutManager(layoutManager);
-                        mRecyclerView.setHasFixedSize(true);
-                    }
-                });
+                ResponseBody responseBody = response.peekBody(Long.MAX_VALUE);
+                String jsonData = responseBody.string();
+                Log.e(TAG, jsonData);
+                if (response.isSuccessful()){
+                    recipesList = RecipeService.processResults(response);
+                    RecipesActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mAdapter = new RecipeListAdapter(getApplicationContext(), recipesList);
+                            mRecyclerView.setAdapter(mAdapter);
+                            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(RecipesActivity.this);
+                            mRecyclerView.setLayoutManager(layoutManager);
+                            mRecyclerView.setHasFixedSize(true);
+                            if (jsonData.length() < 50){
+                                Toast.makeText(RecipesActivity.this,"Error !!!\nRecipe not found please try again" ,Toast.LENGTH_LONG).show();
+                                mRecipeKeyedResult.setTextColor(Color.parseColor("red"));
+                                mRecipeKeyedResult.setText("Recipe not found please try again!!!");
+                                Intent intent = new Intent(RecipesActivity.this,MainActivity.class);
+                                startActivity(intent);
+                            }
+                            hideProgressDialog();
+                        }
+                    });
+                }
             }
         });
+    }
+
+    public void showProgressDialog(){
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+    public void hideProgressDialog(){
+        mProgressBar.setVisibility(View.INVISIBLE);
     }
 }
